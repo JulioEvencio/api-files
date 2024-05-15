c9fd01218df2fed18c14d81fa5518850804d3aa5
JulioEvencio <julioigreja.dev@gmail.com>
2024-05-15 17:16:52 -0300

    added: project base

---
+++ api-files/src/main/java/com/github/julioevencio/apifiles/ApiFilesApplication.java
@@ -0,0 +1,13 @@
+package com.github.julioevencio.apifiles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiFilesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiFilesApplication.class, args);
+	}
+
+}
---
+++ api-files/src/test/java/com/github/julioevencio/apifiles/ApiFilesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.julioevencio.apifiles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiFilesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
