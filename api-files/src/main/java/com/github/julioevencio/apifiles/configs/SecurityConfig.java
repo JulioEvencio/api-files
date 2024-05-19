7938a0b197081e13ce2fc23d4dd40b87d3a1454d
JulioEvencio <julioigreja.dev@gmail.com>
2024-05-18 22:33:56 -0300

    update: fix bug - ath

--- api-files/src/main/java/com/github/julioevencio/apifiles/configs/SecurityConfig.java
+++ api-files/src/main/java/com/github/julioevencio/apifiles/configs/SecurityConfig.java
@@ -69,8 +69,8 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/v3/api-docs/**", "/swagger-ui/**").permitAll()
                         .requestMatchers(HttpMethod.POST, "/v1/api/auth/register", "/v1/api/auth/login").permitAll()
-                        .requestMatchers(HttpMethod.GET, "/v1/api/files/**").authenticated()
-                        .requestMatchers(HttpMethod.POST, "/v1/api/files").authenticated()
+                        .requestMatchers(HttpMethod.GET, "/v1/api/files/download", "/v1/api/files/backup").authenticated()
+                        .requestMatchers(HttpMethod.POST, "/v1/api/files/upload").authenticated()
                         .anyRequest().denyAll()
                 )
                 .addFilterBefore(jwtFilter(), UsernamePasswordAuthenticationFilter.class)
